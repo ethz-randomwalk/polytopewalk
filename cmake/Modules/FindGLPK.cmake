f309ba98f569b430b2cee8f3151f302a4d69ff6b
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-11-05 14:54:37 +0100

    test FindGLPK

--- cmake/Modules/FindGLPK.cmake
+++ cmake/Modules/FindGLPK.cmake
@@ -1,10 +1,12 @@
 find_path(GLPK_INCLUDE_DIR glpk.h
     PATHS
     /mingw64/include         # Windows MSYS2 path for headers
+    D:/a/_temp/msys64/mingw64/include
 )
 find_library(GLPK_LIBRARY NAMES glpk
     PATHS
     /mingw64/lib             # Windows MSYS2 path for libraries
+    D:/a/_temp/msys64/mingw64/lub
 )
 
 if(GLPK_INCLUDE_DIR AND GLPK_LIBRARY)
