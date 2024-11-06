e6a19dfaa796a0c04588684b735914b2d15127ec
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-11-06 23:25:57 +0100

    1.0.4

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 cmake -B build -S .
 # rm -r build; mkdir build ; cd build; cmake .. 
 # get source distribution
-# python -m build --sdist
+# python3 -m build --sdist
+# upload to pypi
+# python3 -m twine upload dist/*
